a88f81f3bd746fceaa982896b564ea0894435ae3
dapletch <seth.pletcher@gmail.com>
2016-12-26 18:16:06 -0500

    Accounted for nothing being entered into main as an argument. An error message will display if nothing is entered.

--- src/main/java/com/deleteduplicates/Main.java
+++ src/main/java/com/deleteduplicates/Main.java
@@ -13,12 +13,16 @@ public class Main {
 
     public static void main(String[] args) {
 
-        File file = new File(args[0]);
-        // Need to validate the input for the file containing the list of duplicate files.
-        if (!DeleteDuplicateUtils.isFileValid(file)) {
-            logger.error("The file that inputted was not valid: ", file);
+        if (args.length > 0) {
+            File file = new File(args[0]);
+            // Need to validate the input for the file containing the list of duplicate files.
+            if (!DeleteDuplicateUtils.isFileValid(file)) {
+                logger.error("The file that inputted was not valid: ", file);
+            }
+            ReadFileCreateArrayList readFileCreateArrayList = new ReadFileCreateArrayList();
+            readFileCreateArrayList.createArrayListDeleteDuplicates(file);
+        } else {
+            logger.error("There was no file to be inputted. Please select a file and try again.");
         }
-        ReadFileCreateArrayList readFileCreateArrayList = new ReadFileCreateArrayList();
-        readFileCreateArrayList.createArrayListDeleteDuplicates(file);
     }
 }
