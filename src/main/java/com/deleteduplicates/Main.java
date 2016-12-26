eb70bfe0248879e8859eaf3bf45a538e46d297fd
dapletch <seth.pletcher@gmail.com>
2016-12-26 14:12:47 -0500

    The initial commit the delete duplicate file project. This project will be created as an executable jar that will traverse the directories specified in the file that this program will take as an argument. This program will delete duplicates based off the file name which will allow provide much needed disk space.

---
+++ src/main/java/com/deleteduplicates/Main.java
@@ -0,0 +1,9 @@
+package com.deleteduplicates;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // Need to validate the input for the file containing the list of duplicate files.
+
+    }
+}
---
+++ src/main/java/com/deleteduplicates/utils/DeleteDuplicateUtils.java
@@ -0,0 +1,7 @@
+package com.deleteduplicates.utils;
+
+/**
+ * Created by Seth on 12/26/2016.
+ */
+public class DeleteDuplicateUtils {
+}
